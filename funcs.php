3660c8b1fdd6c83b869e525d118a10667785a363
DatDraggy <spamme@kieran.pw>
2018-05-29 15:03:21 +0200

    Yee

--- funcs.php
+++ funcs.php
@@ -3,6 +3,7 @@ function sendMessage($chatId, $text) {
   global $config;
   $url = 'https://api.telegram.org/bot' . $config['token'] . '/';
   $response = file_get_contents($url . 'sendMessage?disable_web_page_preview=true&parse_mode=html&chat_id=' . $chatId . '&text=' . urlencode($text));
+  //Might use http_build_query in the future
 }
 
 function getCurrentReward() {
