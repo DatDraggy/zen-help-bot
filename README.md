f4e09b5ebeaaa9216c395d799ee6c15a6230fe72
DatDraggy <marlinvk92@gmail.com>
2018-06-13 18:03:22 +0200

    Donations

--- README.md
+++ README.md
@@ -13,3 +13,9 @@
 - [X] Thanks scoreboard
 - [ ] /community: Discord twitter etc
 - [ ] Roi calculator
+
+
+#### Donations
+ZenCash zni7tRLevBnJxWMzkUoMVze1e6RCSPDdbfw
+BTC 122WSgrn2YVG6KQSKB53jfCaZYi3xMi6nb
+ETH 0x4b39187EBBb674Fb659A81a433D8e8AfbE3aA32b
--- webhook.php
+++ webhook.php
@@ -57,7 +57,9 @@ switch ($command) {
 I\'m the ZenCash Help Bot. I can provide quick information about topics when I see a command that I know.
 To get a list of all commands I know, simply send /zencommands to me.
 
-I am also open source, so if you like you can add your own commands by creating a pull request here: https://github.com/DatDraggy/zencash-help-bot');
+I am also open source, so if you like you can add your own commands by creating a pull request here: https://github.com/DatDraggy/zencash-help-bot
+
+If you would like to donate, this is my ZenCash address: zni7tRLevBnJxWMzkUoMVze1e6RCSPDdbfw');
     }
     break;
 
