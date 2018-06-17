0acc2879b05ad5727b5014c925e75650a483618a
DatDraggy <spamme@kieran.pw>
2018-06-17 14:27:40 +0200

    Port

--- funcs.php
+++ funcs.php
@@ -73,7 +73,7 @@ function getAdmins($chatId) {
 function buildDatabaseConnection($config) {
   //Connect to DB only here to save response time on other commands
   try {
-    $dbConnection = new PDO('mysql:dbname=' . $config['dbname'] . ';host=' . $config['dbserver'] . ';charset=utf8mb4', $config['dbuser'], $config['dbpassword']);
+    $dbConnection = new PDO('mysql:dbname=' . $config['dbname'] . ';host=' . $config['dbserver'] . ';port=' . $config['dbport'] . ';charset=utf8mb4', $config['dbuser'], $config['dbpassword']);
     $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $e) {
