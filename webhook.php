4ec9c6332df2db34d80bd6bb03cda3be30ee39dd
DatDraggy <spamme@kieran.pw>
2018-06-17 14:23:48 +0200

    test

--- webhook.php
+++ webhook.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . "/funcs.php");
 require_once(__DIR__ . "/config.php");
 require_once(__DIR__ . "/infos.php");
-
+//test
 $response = file_get_contents('php://input');
 $data = json_decode($response, true);
 $dump = print_r($data, true);
