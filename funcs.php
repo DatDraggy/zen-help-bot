2579eeb5408e473e78ae44b936ab44c695af5a5f
DatDraggy <marlinvk92@gmail.com>
2018-07-19 23:03:45 +0200

    Fix

--- funcs.php
+++ funcs.php
@@ -88,7 +88,7 @@ function getAdmins($chatId) {
 function buildDatabaseConnection($config) {
   //Connect to DB only here to save response time on other commands
   try {
-    $dbConnection = new PDO('mysql:dbname=' . $config['dbname'] . ';host=' . $config['dbserver'] . ';port=' . $config['dbport'] . ';charset=utf8mb4', $config['dbuser'], $config['dbpassword']);
+    $dbConnection = new PDO('mysql:dbname=' . $config['dbname'] . ';host=' . $config['dbserver'] . ';port=' . $config['dbport'] . ';charset=utf8mb4', $config['dbuser'], $config['dbpassword'], array(PDO::ATTR_TIMEOUT => 30));
     $dbConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $dbConnection->setAttribute(PDO::ATTR_TIMEOUT, 30);
