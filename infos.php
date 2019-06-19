420ebdde9dd314b6478d55d938b04456e102b932
DatDraggy <bmxstreet@gmx.net>
2019-06-19 22:48:21 +0200

    Update infos.php

--- infos.php
+++ infos.php
@@ -3,7 +3,7 @@
 $infos['groups'] = 'Official Info: @horizenglobal
 English: @horizencommunity and @ZenCash
 Russian: @horizenru
-Spanish: @zencashlatinoamerica
+Portuguese: @HorizenPortuguese
 Chinese(Telegram): @ZenCashZh
 Chinese(WeChat): <a href="https://u.wechat.com/IFl8PTtHf7YmHP34XqbI6As">https://u.wechat.com/IFl8PTtHf7YmHP34XqbI6As</a>
 International Discord: https://discord.gg/tmFQmfw
